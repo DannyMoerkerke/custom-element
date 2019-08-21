7aa2a0d91443c261ade83a8029bce5fc47a74b7f
snieuwenhuis@bol.com <stefannhs@gmail.com>
2019-08-21 15:57:12 +0200

    update rollup config

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+export default {
+    input: 'src/custom-element.js',
+    output: [
+        {
+            file: 'dist/custom-element.js',
+            format: 'iife',
+            name: 'CustomElement'
+        },
+        {
+            file: 'dist/custom-element.es.js',
+            format: 'es'
+        },
+        {
+            file: 'dist/custom-element.umd.js',
+            format: 'umd',
+            name: 'CustomElement'
+        }
+    ]
+}
